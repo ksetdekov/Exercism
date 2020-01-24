37a15c96078652cdfa88c6f25ab256a7b7b2c71f
Kirill Setdekov <ksetdekov@gmail.com>
2020-01-24 09:57:09 +0300

    did hamming code

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,4 +1,11 @@
-# This is a stub function to take two strings
-# and calculate the hamming distance
 hamming <- function(strand1, strand2) {
+    tovector <- function(strand) {
+        unlist(strsplit(strand, NULL))
+    }
+    strand1vector <- tovector(strand1)
+    strand2vector <- tovector(strand2)
+    
+    if (length(strand1vector) != length(strand2vector))
+        stop('input not positive')
+    sum(strand1vector != strand2vector)
 }
