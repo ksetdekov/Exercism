dc3b41ffbe9175bff93deb8269569eae59a800ac
ksetdekov <ksetdekov@gmail.com>
2020-01-26 23:56:30 +0300

    changed to fail faster

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,11 +1,10 @@
 hamming <- function(strand1, strand2) {
+    if (nchar(strand1) != nchar(strand2))
+        stop('input not positive')
     tovector <- function(strand) {
         unlist(strsplit(strand, NULL))
     }
     strand1vector <- tovector(strand1)
     strand2vector <- tovector(strand2)
-    
-    if (length(strand1vector) != length(strand2vector))
-        stop('input not positive')
     sum(strand1vector != strand2vector)
 }
