07c7367c11522ec1cc4d149ad69611a246e0cf11
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-03 19:25:53 +0300

    finished amino convert

--- r/rna-transcription/rna-transcription.R
+++ r/rna-transcription/rna-transcription.R
@@ -1,3 +1,10 @@
 to_rna <- function(dna) {
-  
+    converter <- function(amino) {
+        if (!(amino %in% c("C", "G", "T", "A")))
+            stop('unallowed input characters')
+        c("G", "C", "A", "U")[match(amino, c("C", "G", "T", "A"))]
+    }
+    paste0(unlist(lapply(unlist(
+        strsplit(dna, NULL)
+    ), converter)), collapse = '')
 }
