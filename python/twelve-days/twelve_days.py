e08a0514c02795b170c93e179c33d29a013895f3
Kirill Setdekov <ksetdekov@gmail.com>
2020-05-04 13:37:51 +0300

    it counts the days!

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,2 +1,12 @@
+def day(count):
+    return ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh",
+            "twelfth"][count - 1]
+
+
 def recite(start_verse, end_verse):
-    pass
+    for v in range(start_verse, end_verse + 1):
+        print(day(v))
+
+
+for i in range(1, 13):
+    recite(i, i)
