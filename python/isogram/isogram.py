ddc05e74f279b8a86dad29ca4567a557cac31be7
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-05-25 18:11:24 +0300

    did based on counting letters

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,10 @@
 def is_isogram(string):
-    pass
+    string = string.lower().replace(" ", "").replace("-", "")
+    counts = dict()
+    for letter in string:
+        counts[letter] = counts.get(letter, 0) + 1
+    bigcount = 1
+    for wrd, cnt in counts.items():
+        if cnt > bigcount:
+            bigcount = cnt
+    return bigcount == 1
