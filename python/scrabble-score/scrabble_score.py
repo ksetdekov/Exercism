9fe1295ed3c4c0947ddaef427fe9769e43b70ebe
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-05-26 10:36:48 +0300

    a long dictionary and a simple code for scrabble_score.py

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,12 @@
 def score(word):
-    pass
+    converter = dict.fromkeys(['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'], 1)
+    converter.update(dict.fromkeys(['d', 'g'], 2))
+    converter.update(dict.fromkeys(['b', 'c', 'm', 'p'], 3))
+    converter.update(dict.fromkeys(['f', 'h', 'v', 'w', 'y'], 4))
+    converter.update({'k': 5})
+    converter.update(dict.fromkeys(['j', 'x'], 8))
+    converter.update(dict.fromkeys(['q', 'z'], 10))
+    output = 0
+    for i in word.lower():
+        output += converter[i]
+    return output
