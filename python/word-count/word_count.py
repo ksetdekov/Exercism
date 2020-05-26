15d17c0fbee40665db69165abc042b2cd22184d7
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-05-26 10:13:41 +0300

    regular expression FTW

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,9 @@
+import re
+
+
 def count_words(sentence):
-    pass
+    words = re.findall(r"[a-z]+'?[a-z]+|[a-z]+|\d+", sentence.lower())
+    counts = dict()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+    return counts
